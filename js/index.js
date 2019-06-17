9f51e368fcf630545b3f2547a62ba885d6e4abd2
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-17 18:27:18 -0400

    created content for nav and top content

--- js/index.js
+++ js/index.js
@@ -40,3 +40,50 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const headerEl = document.querySelector('header');
+headerEl;
+const subHeadline = document.createElement('h1');
+subHeadline;
+subHeadline.textContent = "That is going to change the world!"
+headerEl.appendChild(subHeadline);
+
+const navtext = document.querySelector('nav a');
+const text1 = document.createElement('a');
+text1.textContent = siteContent["nav"]["nav-item-1"];
+navtext.appendChild(text1);
+
+const text2 = document.createElement('a');
+text2.textContent = siteContent["nav"]["nav-item-2"];
+navtext.appendChild(text2);
+
+const text3 = document.createElement('a');
+text3.textContent = siteContent["nav"]["nav-item-3"];
+navtext.appendChild(text3);
+
+const text4 = document.createElement('a');
+text4.textContent = siteContent["nav"]["nav-item-4"];
+navtext.appendChild(text4);
+
+const text5 = document.createElement('a');
+text5.textContent = siteContent["nav"]["nav-item-5"];
+navtext.appendChild(text5);
+
+const text6 = document.createElement('a');
+text6.textContent = siteContent["nav"]["nav-item-6"];
+navtext.appendChild(text6);
+
+const buttontext = document.querySelector('button');
+buttontext.textContent = "Get Started";
+
+const topimg = document.getElementById("cta-img");
+topimg.setAttribute('src', siteContent["cta"]["img-src"])
+
+
+//const toptext = document.getElementsByClassName(['top-content']['text-content']);
+const featureh4 = document.querySelector('h4');
+featureh4.textContent = siteContent["main-content"]["features-h4"];
+
+const featureContent = document.querySelector('p');
+featureContent.textContent = siteContent["main-content"]["features-content"];
+
